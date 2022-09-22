feb5a7f3f27e8842aa2d815d9478a8c65bc6f419
BobbyDMullinsIII <bobbymullins20000@gmail.com>
2022-09-22 01:28:50 -0400

    Create project1.cc

---
+++ project1.cc
@@ -0,0 +1,20 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Solution/Project:   Project 1 (Mainline Team 1)
+//	File Name:          project1.cc
+//	Description:        Main file for Operating Systems Mainline Project 1
+//	Course:             CSCI-4727-001 - Operating Systems
+//	Authors:            Bobby Mullins, Isaiah Jayne, Jacob Manis, Zachary Lykins       
+//	Created:            Wednesday, September  21, 2022
+//	Copyright:          Mainline Team 1 - Operating Systems
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  
+  return 0;
+}
