abf3d74d7b7a924680a0f868dd1271221e9fdbf7
BobbyDMullinsIII <bobbymullins20000@gmail.com>
2022-09-28 19:50:27 -0400

    Finished config input functionality
    -Finished method for inserting trace.config file into program
    -Fixed some comments within config explanations

--- TraceConfig.hpp
+++ TraceConfig.hpp
@@ -10,7 +10,7 @@ public:
     ~TraceConfig();
 
     //Data TLB Configuration
-    int numTLBSets;                 //Number of sets within TLB (Max: 8 sets, 1 set = direct mapped, 16 sets = fully associative, Must be power of 2)
+    int numTLBSets;                 //Number of sets within TLB (Max: 16 sets, 1 set = direct mapped, 16 sets = fully associative, Must be power of 2)
     int TLBSetSize;                 //Number of entries in each set (Max: 64 entries, Must be power of 2)
 
     //Page Table Configuration
@@ -21,13 +21,13 @@ public:
     //L1 Cache Configuration
     int L1NumSets;                  //Number of sets in L1 (Max: 8 sets, 1 set = direct mapped, Must be power of 2)
     int L1SetSize;                  //Number of entries in each set in L1 (Max: 128 entries, Must be power of 2)
-    int L1LineSize;                 //Size (in bytes) of each line in L1 (Min: 8 bytes, Must be power of 2)
+    int L1LineSize;                 //Size (in bytes) of each line in L1 (Min: 8 bytes, Max: 64 bytes, Must be power of 2)
     bool L1WriteThrough;            //Switch for L1 to be write-through or not (y/n = true/false)
 
     //L2 Cache Configuration
-    int L2NumSets;                  //Number of sets in L2 (Max: 8 sets, 1 set = direct mapped, Must be power of 2)
+    int L2NumSets;                  //Number of sets in L2 (Max: 16 sets, 1 set = direct mapped, Must be power of 2)
     int L2SetSize;                  //Number of entries in each set in L2 (Max: 512 entries, Must be power of 2)
-    int L2LineSize;                 //Size (in bytes) of each line in L2 (Min: 8 bytes, Must be power of 2)
+    int L2LineSize;                 //Size (in bytes) of each line in L2 (Min: 8 bytes, Max: 64 bytes, Must be power of 2)
     bool L2WriteThrough;            //Switch for L2 to be write-through or not (y/n = true/false)
 
     //Active Modules Configuration
